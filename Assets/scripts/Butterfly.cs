8ea6999b416c9ec90627dd1a91a7acf8967ea340
ringating <34548930+ringating@users.noreply.github.com>
2018-04-09 15:14:29 -0700

    butterfly stuff added
    the handler is probably pointless

---
+++ Assets/scripts/Butterfly.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Butterfly : MonoBehaviour
+{
+
+	private Vector2 ogPosition;
+
+	private float speed;
+	private Vector2 startPosition;
+	private float fallTime;
+
+	public float minFallSpeed;
+	public float maxFallSpeed;
+
+	public float posSelectionRadius;
+
+	public float minFallTime;
+	public float maxFallTime;
+
+	public SpriteRenderer visuals;
+
+	void Start ()
+	{
+		ogPosition = new Vector2(transform.position.x, transform.position.y);
+		Flap();
+	}
+	
+	void Update ()
+	{
+		if (fallTime > 0)
+		{
+			fallTime -= Time.deltaTime;
+			transform.position = new Vector3(transform.position.x, transform.position.y - Mathf.Abs(speed)*Time.deltaTime, transform.position.z);
+		}
+		else
+		{
+			Flap();
+		}
+	}
+
+	private void Flap ()
+	{
+		startPosition = ogPosition + Random.insideUnitCircle * posSelectionRadius;
+		speed = minFallSpeed + Random.value * (maxFallSpeed - minFallSpeed);
+		fallTime = minFallTime + Random.value * (maxFallTime - minFallTime);
+
+		transform.position = new Vector3(startPosition.x, startPosition.y, transform.position.z);
+	}
+
+	public void Show(bool enable)
+	{
+		visuals.enabled = enable;
+	}
+}
---
+++ Assets/scripts/ButterflyHandler.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButterflyHandler : MonoBehaviour
+{
+
+	public Butterfly b1;
+	public Butterfly b2;
+	public Butterfly b3;
+
+	void Start ()
+	{
+		
+	}
+	
+	void Update ()
+	{
+		
+	}
+}
