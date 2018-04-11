7da42f9ca77463d6722868352127eba8bcb91caf
ringating <34548930+ringating@users.noreply.github.com>
2018-04-11 15:12:15 -0700

    menu stuff
    haven't tested yet, but it's added

---
+++ Assets/menu/MenuManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour 
+{
+	public void ToGame()
+	{
+		SceneManager.LoadScene ("ice demo");
+	}
+
+	public void ToAbout()
+	{
+		SceneManager.LoadScene ("about");
+	}
+
+	public void Quit()
+	{
+		Application.Quit ();
+	}
+
+
+}
