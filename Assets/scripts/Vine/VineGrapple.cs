b0f8914bc73af15816de0e97d6a44981849ff87f
ringating <ringatingaling@gmail.com>
2018-04-18 01:45:51 -0700

    added reticle, various grapple fixes
    also a new exception being thrown by the grapple, so that's ~cool~

--- Assets/scripts/Vine/VineGrapple.cs
+++ Assets/scripts/Vine/VineGrapple.cs
@@ -7,6 +7,7 @@ public class VineGrapple : MonoBehaviour
 	public float speed;
 	public float pullSpeed;
 	public float minGrappleLength;
+	public float maxGrappleLength;
 	private Vector2 speedVec;
 	public CharState cs;
 
@@ -37,8 +38,17 @@ public class VineGrapple : MonoBehaviour
 	{
 		if (targetPoint != null && cs.ctrl.disable)
 		{
+			// has hooked
 			cs.ctrl.rigid.velocity = (targetPoint - cs.ctrl.rigid.position).normalized * pullSpeed;
 			if (Vector2.Distance(targetPoint, cs.ctrl.rigid.position) < minGrappleLength)
+			{
+				Delete();
+			}
+		}
+		else
+		{
+			// hasn't hooked
+			if (Vector2.Distance(rigid.position, cs.ctrl.rigid.position) > maxGrappleLength)
 			{
 				Delete();
 			}
