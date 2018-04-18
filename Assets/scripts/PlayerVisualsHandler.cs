2dbc567494f8b682146bef90bc59b999772161dc
ringating <34548930+ringating@users.noreply.github.com>
2018-04-18 12:32:04 -0700

    added character visuals swapping
    neat

---
+++ Assets/scripts/PlayerVisualsHandler.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerVisualsHandler : MonoBehaviour
+{
+	public CharState cs;
+
+	public GameObject neutralVisuals;
+	public GameObject iceVisuals;
+	public GameObject grassVisuals;
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		if (cs.state == 0)
+		{
+			neutralVisuals.SetActive(true);
+			iceVisuals.SetActive(false);
+			grassVisuals.SetActive(false);
+		}
+		else if (cs.state == 2)
+		{
+			neutralVisuals.SetActive(false);
+			iceVisuals.SetActive(true);
+			grassVisuals.SetActive(false);
+		}
+		else if (cs.state == 3)
+		{
+			neutralVisuals.SetActive(false);
+			iceVisuals.SetActive(false);
+			grassVisuals.SetActive(true);
+		}
+	}
+}
