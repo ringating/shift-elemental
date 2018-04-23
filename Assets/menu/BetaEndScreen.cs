7255d3f97a5d2bf7760dea7291a029582b68f364
ringating <ringatingaling@gmail.com>
2018-04-23 05:49:53 -0700

    added end screen
    also removed the "about" scene from the build, since it's no longer necessary (nor up to date)

---
+++ Assets/menu/BetaEndScreen.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BetaEndScreen : MonoBehaviour
+{
+	public KickBackToMenu kick;
+	public SpriteRenderer endScreen;
+
+	void Start ()
+	{
+		kick.enabled = false;
+		endScreen.enabled = false;
+	}
+
+	private void OnTriggerEnter2D(Collider2D collision)
+	{
+		if (collision.tag == "Player")
+		{
+			collision.gameObject.SetActive(false);
+			kick.enabled = true;
+			endScreen.enabled = true;
+		}
+	}
+}
