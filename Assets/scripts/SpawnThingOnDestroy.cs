3125fc00af96157fa1bdecd165ef429276be5e56
ringating <34548930+ringating@users.noreply.github.com>
2018-04-25 15:14:57 -0700

    new script

---
+++ Assets/scripts/SpawnThingOnDestroy.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnThingOnDestroy : MonoBehaviour
+{
+	public GameObject thing;
+
+	private void OnDestroy()
+	{
+		Instantiate(thing, transform.position, transform.rotation);
+	}
+}
