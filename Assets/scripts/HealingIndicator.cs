dfcdc425ca4cff19b486064a3db5cbc6e29c2709
ringating <ringatingaling@gmail.com>
2018-04-29 22:11:06 -0700

    added health ui
    and disabled the debug ui

---
+++ Assets/scripts/HealingIndicator.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealingIndicator : MonoBehaviour
+{
+	private SpriteRenderer sr;
+	private float prevHealth;
+	public CharState cs;
+
+	// public float minOpacity;
+	// public float pulsePeriod;
+
+	public float pulseSpeedMultiplier;
+
+
+	void Start ()
+	{
+		sr = GetComponent<SpriteRenderer>();
+	}
+
+	void Update ()
+	{
+		if (cs.health > prevHealth)
+		{
+			sr.enabled = true;
+		}
+		else
+		{
+			sr.enabled = false;
+		}
+
+		sr.color = new Color(1, 1, 1, (Mathf.Sin(Time.time*pulseSpeedMultiplier)+1)/2);
+
+		prevHealth = cs.health;
+	}
+}
---
+++ Assets/scripts/HealthUIHandler.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthUIHandler : MonoBehaviour
+{
+	public CharState cs;
+
+	public Sprite flowerOpen;
+	public Sprite flowerClosed;
+	public Sprite flowerTween;
+
+	public SpriteRenderer[] flowers;
+
+	private float incr;
+
+	private void Start ()
+	{
+		incr = cs.maxHealth / flowers.Length;
+	}
+
+	private void Update ()
+	{
+		for (int i = 0; i < flowers.Length; i++)
+		{
+			if (cs.health >= (i + 1) * incr)
+			{
+				flowers[i].sprite = flowerOpen;
+			}
+			else if (cs.health >= i * incr)
+			{
+				flowers[i].sprite = flowerTween;
+			}
+			else
+			{
+				flowers[i].sprite = flowerClosed;
+			}
+		}
+	}
+}
