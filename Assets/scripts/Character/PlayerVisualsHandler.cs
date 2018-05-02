cd90ae6d1ebdcd4603045fb68d9a13a51696ea64
ringating <34548930+ringating@users.noreply.github.com>
2018-05-02 15:19:25 -0700

    added hair hue changing to visuals handler

--- Assets/scripts/Character/PlayerVisualsHandler.cs
+++ Assets/scripts/Character/PlayerVisualsHandler.cs
@@ -10,6 +10,12 @@ public class PlayerVisualsHandler : MonoBehaviour
 	public EntityRenderer neutralVisuals;
 	public SpriteRenderer iceVisuals;
 	public SpriteRenderer grassVisuals;
+
+	public SpriteRenderer[] hairPieces;
+
+	public Color iceHue;
+	public Color grassHue;
+	
 	
 	// Update is called once per frame
 	void Update ()
@@ -19,18 +25,30 @@ public class PlayerVisualsHandler : MonoBehaviour
 			//neutralVisuals.enabled = true;
 			iceVisuals.enabled = false;
 			grassVisuals.enabled = false;
+			for (int i = 0; i < hairPieces.Length; i++)
+			{
+				hairPieces[i].color = Color.white;
+			}
 		}
 		else if (cs.state == 2)
 		{
 			//neutralVisuals.enabled = false;
 			iceVisuals.enabled = true;
 			grassVisuals.enabled = false;
+			for (int i = 0; i < hairPieces.Length; i++)
+			{
+				hairPieces[i].color = iceHue;
+			}
 		}
 		else if (cs.state == 3)
 		{
 			//neutralVisuals.enabled = false;
 			iceVisuals.enabled = false;
 			grassVisuals.enabled = true;
+			for (int i = 0; i < hairPieces.Length; i++)
+			{
+				hairPieces[i].color = grassHue;
+			}
 		}
 	}
 }
