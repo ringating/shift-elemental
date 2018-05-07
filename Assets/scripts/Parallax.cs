027fc75b3bc7abc983d2aff67999da122c22060e
ringating <ringatingaling@gmail.com>
2018-05-06 23:41:18 -0700

    added preliminary parallax script

---
+++ Assets/scripts/Parallax.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Parallax : MonoBehaviour
+{
+	public GameObject primarySection; // the one closest to center screen
+	public GameObject secondarySection; // the other one
+	private GameObject temp; // for swapping primary and secondary
+
+	public float sectionWidth;
+
+	void Start ()
+	{
+		
+	}
+	
+	void Update ()
+	{
+		
+	}
+}
