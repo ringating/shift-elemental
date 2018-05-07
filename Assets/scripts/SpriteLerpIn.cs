edb309fd892179ec6997f385f04d085a775fdf1e
ringating <ringatingaling@gmail.com>
2018-05-06 22:55:33 -0700

    added a scale lerp script
    that isn't used in any real way yet

---
+++ Assets/scripts/SpriteLerpIn.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpriteLerpIn : MonoBehaviour
+{
+	// this script should not be used with colliders or rigidbodies, as it affects the actual transform scale and will make collision and physics weird as a result.
+
+	public float lerpAlpha;
+
+	private Vector3 targetScale;
+	private float scale = 0;
+
+	void Start ()
+	{
+		targetScale = transform.localScale;
+		transform.localScale = Vector3.zero;
+	}
+	
+	void Update ()
+	{
+		transform.localScale = targetScale * scale;
+		scale = Mathf.Lerp(scale, 1, Mathf.Clamp01(lerpAlpha*Time.deltaTime));
+	}
+}
