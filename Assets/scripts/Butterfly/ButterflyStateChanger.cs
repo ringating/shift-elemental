aa3c7af82bd1335bc54b64ae1d08004d920bb2c5
ringating <34548930+ringating@users.noreply.github.com>
2018-05-07 13:23:42 -0700

    added butterflyStateChanger
    for changing the butterfly sprites according to state

---
+++ Assets/scripts/Butterfly/ButterflyStateChanger.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButterflyStateChanger : MonoBehaviour
+{
+	public CharState cs;
+
+	public Sprite iceUp;
+	public Sprite iceDown;
+	public Sprite iceTween;
+	public Sprite grassUp;
+	public Sprite grassDown;
+	public Sprite grassTween;
+
+	public ButterflySpritePicker[] bsp;
+	
+	void Update ()
+	{
+		if (cs.state == 2)
+		{
+			foreach (ButterflySpritePicker b in bsp)
+			{
+				b.movingUp = iceUp;
+				b.movingDown = iceDown;
+				b.tween = iceTween;
+			}
+		}
+		else if (cs.state == 3)
+		{
+			foreach (ButterflySpritePicker b in bsp)
+			{
+				b.movingUp = grassUp;
+				b.movingDown = grassDown;
+				b.tween = grassTween;
+			}
+		}
+	}
+}
