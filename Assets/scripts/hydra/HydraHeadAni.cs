2c548f69fb440098f210be11fae2122019a86647
ringating <ringatingaling@gmail.com>
2018-05-15 07:17:44 -0700

    hydra animates properly
    doesnt actually attack, isnt actually killable, doesnt have any sounds, doesnt activate by range
    
    but hey, it moves

---
+++ Assets/scripts/hydra/HydraHeadAni.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HydraHeadAni : MonoBehaviour
+{
+
+	public float cooldown;
+	private float timer;
+
+	private bool inRange = true;
+
+	public Animator ani;
+
+	public Transform player;
+	public float activeRange;
+
+	void Start ()
+	{
+		timer = cooldown;
+	}
+	
+	void Update ()
+	{
+		ani.SetInteger("state", 0);
+
+		timer -= Time.deltaTime;
+
+		if (timer <= 0)
+		{
+			if (inRange)
+			{
+				Debug.Log("in range");
+				// do a thing
+				switch (Random.Range((int)0, (int)5))
+				{
+					case 0:
+						Debug.Log(name + ", idle");
+						break;
+
+					case 1:
+						Debug.Log(name + ", wiggle");
+						ani.SetInteger("state",1);
+						break;
+
+					case 2:
+						Debug.Log(name + ", lean forward");
+						ani.SetInteger("state", 2);
+						break;
+
+					case 3:
+						Debug.Log(name + ", lean back");
+						ani.SetInteger("state", 3);
+						break;
+
+					case 4:
+						Debug.Log(name + ", roar");
+						ani.SetInteger("state", 4);
+						break;
+
+					default:
+						break;
+				}
+			}
+
+			timer = cooldown;
+		}
+	}
+}
---
+++ Assets/scripts/hydra/MoveBySine.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveBySine : MonoBehaviour
+{
+	// this script locks the transform's local position, so it needs to have a moveable parent if you want to move it elsewhere
+
+	public float xRadius;
+	public float yRadius;
+	public float xSpeedScalar;
+	public float ySpeedScalar;
+	public bool moveInX;
+	public bool moveInY;
+
+	private float x = 0;
+	private float y = 0;
+	
+	private void Update()
+	{
+		if (moveInX)
+		{
+			x = Mathf.Sin(Time.time * xSpeedScalar) * xRadius;
+		}
+		else
+		{
+			x = 0;
+		}
+
+		if (moveInX)
+		{
+			y = Mathf.Sin(Time.time * ySpeedScalar) * yRadius;
+		}
+		else
+		{
+			y = 0;
+		}
+
+		transform.localPosition = new Vector3(x,y,transform.localPosition.z);
+	}
+}
